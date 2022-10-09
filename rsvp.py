c9278f3cf596bfecb6c1de7c064fa63229d6a14b
dbha <dbha0719@gmail.com>
2022-10-09 17:40:25 +0900

    update

--- rsvp.py
+++ rsvp.py
@@ -120,4 +120,4 @@ def api_rsvp(id):
         return json.dumps({"deleted": "true"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='8080', debug=True)
+    app.run(host='0.0.0.0', port='5000', debug=True)
